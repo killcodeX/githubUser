8adb8332a1409f3d7b0823aaf459137df1fbca65
Aaquib Ahmed <aaquibskimbox@gmail.com>
2020-10-14 02:00:18 +0530

    donut configuration done

--- src/components/Charts/Doughnut2d.js
+++ src/components/Charts/Doughnut2d.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import ReactFC from "react-fusioncharts";
+import FusionCharts from "fusioncharts";
+import Column2D from "fusioncharts/fusioncharts.charts";
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.candy";
 
-const Doughnut2d = () => {
-  return <div>chart</div>;
+ReactFC.fcRoot(FusionCharts, Column2D, FusionTheme);
+
+const Doughnut2d = ({data}) => {
+  const chartConfigs = {
+    type: "doughnut2d", 
+    width: "100%", 
+    height: "400", 
+    dataFormat: "json", 
+    dataSource: {
+      // Chart Configuration
+      chart: {
+        caption: "Stars Per Language",     
+        theme: "candy",
+        decimals:0,
+        doughnutRadius: '45%',
+        showPercentValues: 0,
+        paletteColors:'#ff6361, #ffa600, #003f5c, #58508d, #bc5090 '              
+      },
+      data:data,
+    }
+  };
+    return <ReactFC {...chartConfigs} />;
 };
 
 export default Doughnut2d;
--- src/components/Charts/Pie3D.js
+++ src/components/Charts/Pie3D.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import ReactFC from "react-fusioncharts";
+import FusionCharts from "fusioncharts";
+import Column2D from "fusioncharts/fusioncharts.charts";
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
 
-const Pie3D = () => {
-  return <div>chart</div>;
+ReactFC.fcRoot(FusionCharts, Column2D, FusionTheme);
+
+const Pie3D = ({data}) => {
+
+const chartConfigs = {
+  type: "pie3d", 
+  width: "400", 
+  height: "400", 
+  dataFormat: "json", 
+  dataSource: {
+    // Chart Configuration
+    chart: {
+      caption: "Most Languages Used",     
+      theme: "fusion",
+      decimals:0,
+      pieRadius: '45%',
+      paletteColors:'#ff6361, #ffa600, #003f5c, #58508d, #bc5090 '              
+    },
+    data: data
+  }
+};
+  return <ReactFC {...chartConfigs} />;
 };
 
 export default Pie3D;
