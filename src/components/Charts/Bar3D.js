f1a555626900228c0c9787afdb433d0174d69540
Aaquib Ahmed <aaquibskimbox@gmail.com>
2020-10-18 03:12:30 +0530

    chart data is done

--- src/components/Charts/Bar3D.js
+++ src/components/Charts/Bar3D.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import ReactFC from "react-fusioncharts";
+import FusionCharts from "fusioncharts";
+import Column2D from "fusioncharts/fusioncharts.charts";
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
 
-const Bar3D = () => {
-  return <div>chart</div>;
+ReactFC.fcRoot(FusionCharts, Column2D, FusionTheme);
+
+const Bar3D = ({data}) => {
+
+  const chartConfigs = {
+    type: "bar3d", 
+    width: "100%", 
+    height: "400", 
+    dataFormat: "json", 
+    dataSource: {
+      // Chart Configuration
+      chart: {
+        caption: "Most Forked",
+        yAxisName: "Forks",
+        xAxisName: "Repos",
+        yAxisNameFontSize: '16px',
+        xAxisNameFontSize: '16px',     
+        theme: "fusion",
+        decimals:0,
+        paletteColors:'#ff6361, #ffa600, #366EEA, #58508d, #bc5090 '           
+      },
+      data: data
+    }
+  };
+
+  return <ReactFC {...chartConfigs} />;
 };
 
 export default Bar3D;
--- src/components/Charts/Column3D.js
+++ src/components/Charts/Column3D.js
@@ -1,7 +1,36 @@
 import React from 'react';
+import ReactFC from "react-fusioncharts";
+import FusionCharts from "fusioncharts";
+import Column2D from "fusioncharts/fusioncharts.charts";
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
 
-const Column3D = () => {
-  return <div>chart</div>;
+ReactFC.fcRoot(FusionCharts, Column2D, FusionTheme);
+
+
+const Column3D = ({data}) => {
+
+  const chartConfigs = {
+    type: "column3d", 
+    width: "100%", 
+    height: "400", 
+    dataFormat: "json", 
+    dataSource: {
+      // Chart Configuration
+      chart: {
+        caption: "Most Popular",
+        yAxisName: "Stars",
+        xAxisName: "Repos",
+        yAxisNameFontSize: '16px',
+        xAxisNameFontSize: '16px',     
+        theme: "fusion",
+        decimals:0,
+        paletteColors:'#366EEA, #ff6361, #39F3BB, #58508d, #bc5090 '           
+      },
+      data: data
+    }
+  };
+
+  return <ReactFC {...chartConfigs} />;
 };
 
 export default Column3D;
