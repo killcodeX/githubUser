9523099be7c3ddea1e8cfb9a3990727d05bdcba7
Aaquib Ahmed <aaquibskimbox@gmail.com>
2020-10-20 05:59:42 +0530

    search component is setup

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,42 @@
-import React from 'react';
-import styled from 'styled-components';
-import { MdSearch } from 'react-icons/md';
-import { GithubContext } from '../context/context';
+import React from "react";
+import styled from "styled-components";
+import { MdSearch } from "react-icons/md";
+import { GithubContext } from "../context/context";
 const Search = () => {
-  return <h2>search component</h2>;
+  const { request, error } = React.useContext(GithubContext);
+
+  const [user, setUser] = React.useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (user) {
+    }
+  };
+
+  return (
+    <section className="section">
+      <Wrapper className="section-center">
+        {error.show && (
+          <ErrorWrapper>
+            <p>{error.msg}</p>
+          </ErrorWrapper>
+        )}
+        <form onSubmit={handleSubmit}>
+          <div className="form-control">
+            <MdSearch />
+            <input
+              type="text"
+              placeholder="enter github user"
+              value={user}
+              onChange={(e) => setUser(e.target.value)}
+            />
+            {request > 0 && <button type="submit">Search</button>}
+          </div>
+        </form>
+        <h3>requests:{request}/60</h3>
+      </Wrapper>
+    </section>
+  );
 };
 
 const Wrapper = styled.div`
--- src/context/context.js
+++ src/context/context.js
@@ -1,26 +1,52 @@
-import React, { useState, useEffect } from 'react';
-import mockUser from './mockData.js/mockUser';
-import mockRepos from './mockData.js/mockRepos';
-import mockFollowers from './mockData.js/mockFollowers';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import mockUser from "./mockData.js/mockUser";
+import mockRepos from "./mockData.js/mockRepos";
+import mockFollowers from "./mockData.js/mockFollowers";
+import axios from "axios";
 
-const rootUrl = 'https://api.github.com';
-
+const rootUrl = "https://api.github.com";
 
 const GithubContext = React.createContext();
 
-const GithubProvider = ({children}) => {
+const GithubProvider = ({ children }) => {
+  const [githubUser, setGithubUser] = useState(mockUser);
+  const [repos, setRepos] = useState(mockRepos);
+  const [followers, setFollowers] = useState(mockFollowers);
 
-    const [githubUser, setGithubUser] = useState(mockUser);
-    const [repos, setRepos] = useState(mockRepos);
-    const [followers, setFollowers] = useState(mockFollowers);
+  // check rate
+  const checkRequest = () => {
+    axios(`${rootUrl}/rate_limit`)
+      .then(({data}) => {
+          let {rate : { remaining },
+        } = data;
 
-    return (
-        <GithubContext.Provider value={{githubUser, repos, followers}}>
-            {children}
-        </GithubContext.Provider>
-    )
+        setrequest(remaining)
+        if(remaining === 0){
+          toggleError(true, 'sorry, youhave exceeded your hourly rate limit !')
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
+  // request loading
+  const [request, setrequest] = useState(0);
+  const [loading, setLoading] = useState(false);
+
+  // error
+  const [error, setError] = useState({show:false, msg:''})
+
+  function toggleError(show, msg){
+    setError({show,msg})
+  }
+
+  // error
+  useEffect(checkRequest, []);
+
+  return (
+    <GithubContext.Provider value={{ githubUser, repos, followers, request, error }}>
+      {children}
+    </GithubContext.Provider>
+  );
 };
 
-
-export {GithubProvider, GithubContext};
+export { GithubProvider, GithubContext };
